9139e32ecd7032b784a788b7d93a79fa9998d449
Gabriele Oliveira <gabrieleoliveiraif2019@gmail.com>
2026-03-17 03:35:22 -0300

    Atualizações visuais concluídas

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -15,30 +15,29 @@ export default function GameList(){
 
   return(
 
-    <div>
+    <div >
 
       {/* LOGO */}
-      <div style={{
-        display:"flex",
-        justifyContent:"center",
-        marginTop:"40px",
-        marginBottom:"30px"
-      }}>
-
-        <img
-          src="/icone.gif"
-          style={{
-            width:"180px"
-          }}
-        />
-
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "40px",
+          marginBottom: "30px"
+        }}
+      >
+        <img src={"/logo-site-panther3.png"} style={{ width: "500px" }} />
       </div>
+
+      <p className="neon-text">
+        Escolha um jogo, aperte play e divirta-se!
+      </p>
 
       {/* GRID DE JOGOS */}
       <div style={{
         display:"grid",
         gridTemplateColumns:"repeat(auto-fill, 250px)",
-        gap:"30px",
+        gap:"15px",
         justifyContent:"center",
         padding:"40px"
       }}>
@@ -56,12 +55,12 @@ export default function GameList(){
               style={{textDecoration:"none",color:"white"}}
             >
 
-              <div style={{textAlign:"center"}}>
+              <div className="game-card" style={{ textAlign: "center" }}>
 
                 <img
                   src={cover}
                   style={{
-                    width:"250px",
+                    width:"200px",
                     borderRadius:"10px",
                     boxShadow:"0 5px 15px rgba(0,0,0,0.5)"
                   }}
@@ -79,7 +78,69 @@ export default function GameList(){
 
         })}
 
+
+
+
       </div>
+
+
+
+          {/* Rodapé */}
+
+        
+            <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "40px",
+          marginBottom: "30px"
+        }}
+      >
+        <img src={"/dk.gif"} style={{ width: "250px" }} />
+      </div>
+
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  justifyContent: "center",
+                  marginTop: "30px",
+                  marginBottom: "30px",
+                  fontSize: "15px"
+                }}
+              >
+                <span>Site desenvolvido por</span>
+        
+                <img
+                  src={
+                    "/icone-panther.png"
+                  }
+                  style={{ width: "35px", height: "24px" }}
+                />
+        
+                <a
+                  href="https://github.com/PantherDesign"
+                  style={{ color: "#e924d9", textDecoration: "none" }}
+                  target="_blank"
+                >
+                  Panther
+                </a> </div>
+
+
+                
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "0px",
+          marginBottom: "0px"
+        }}
+      >
+        <img src={"/icone.gif"} style={{ width: "150px" }} />
+      </div>
+
 
     </div>
 
